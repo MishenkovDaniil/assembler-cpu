e5a7cd81ce8be9a16d94ec9d685275e47627e56f
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-10-14 01:31:48 +0300

    add function for checking if string is only out of spaces

--- asm/asm.h
+++ asm/asm.h
@@ -9,11 +9,12 @@ struct Label
     char name[MAXSIZE][MAXLEN] = {};
 };
 
+int isspace_s (char *arr);
 int is_label_name (Label *label, const char *jmp_name);
 int init_code (char *text, int *op_code, Label *label);
 void print_op_code (FILE *out_file, int *op_code, int number, const int file_id, const int version);
-void assemble_push_arg (char **text, int *op_code, int *ip);
-void assemble_jmp (char **text, int *op_code, int *ip, Label *label, int *label_index);
-void assemble_label (char *cmd, int *ip, Label *label, int *label_index);
+void assemble_push_arg (char **text, int *op_code, int *ip, Label *label, int *label_index);
+void assemble_jmp_arg (char **text, int *op_code, int *ip, Label *label, int *label_index);
+void assemble_label_arg (char *cmd, int *ip, Label *label, int *label_index);
 
 #endif /* ASM_H */
