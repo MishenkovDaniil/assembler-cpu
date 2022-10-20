b0120fdecf9db38888a72373ed9207db211d7d31
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-10-20 18:45:05 +0300

    add in command reading argument for push from stdin

--- cmd.h
+++ cmd.h
@@ -8,21 +8,26 @@ DEF_CMD (POP,  2, 1, push,  {arg = stack_pop (&STACK);
 DEF_CMD (SUB,  3, 0, push, {val_1  = stack_pop (&STACK);
                             stack_push (&STACK, POP - val_1);})
 DEF_CMD (ADD,  4, 0, push, {stack_push (&STACK, POP + POP);})
-DEF_CMD (MULT, 5, 0, push, {stack_push (&STACK, POP * POP);})
-DEF_CMD (DIV,  6, 0, push, {val_1 = POP;
-                            PUSH (POP / val_1);})
+DEF_CMD (MULT, 5, 0, push, {stack_push (&STACK, (int)((int)POP * (int)POP));})
+DEF_CMD (DIV,  6, 0, push, {val_1 = (int)POP;
+                            PUSH ((int)((int)POP / val_1));})
 DEF_CMD (OUT,  7, 0, push, {val_1 = POP;
-                            printf ("\nresult is [%d]\n", val_1);})
+                            fprintf (stderr, "\nresult is [%d]\n", val_1);})
 DEF_CMD (DUP,  8, 0, push, {val_1 = POP;
                             PUSH (val_1);
                             PUSH (val_1);})
 DEF_CMD (CALL, 9, 1, jmp, {process_call_arg (&FUNC, CPU);})//
-DEF_CMD (RET, 10, 0, push, {cpu->ip = stack_pop (&FUNC);})//
+DEF_CMD (RET, 10, 0, push, {int a = stack_pop (&FUNC);
+                            cpu->ip = a;
+                            fprintf (stderr, "ip is [%d] in return\n", cpu->ip);})//
 DEF_CMD (JMP, 11, 1, jmp, {IP = CODE[IP];})
 DEF_JMP (JB,  12, 1, jmp, <)
 DEF_JMP (JBE, 13, 1, jmp, <=)
-DEF_JMP (JA,  14, 1, jmp, >)
-DEF_JMP (JAE, 15, 1, jmp, >=)
-DEF_JMP (JE,  16, 1, jmp, ==)
+DEF_JMP (JA,  19, 1, jmp, >)
+DEF_JMP (JAE, 16, 1, jmp, >=)
+DEF_JMP (JE,  15, 1, jmp, ==)
 DEF_JMP (JNE, 17, 1, jmp, !=)
-DEF_JMP (JT,  18, 1, jmp, =)
+DEF_JMP (JT,  18, 1, jmp, ==)
+DEF_CMD (IN,  14, 0, push, {int a = 0;
+                            scanf ("%d", &a);
+                            PUSH (a);})
