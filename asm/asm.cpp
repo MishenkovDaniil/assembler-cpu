a5e83dd6b265b02029129f031bd464524f146721
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-10-22 15:27:56 +0300

    update assembling push argument func

--- asm/asm.cpp
+++ asm/asm.cpp
@@ -3,27 +3,11 @@
 
 #include "..\calc.h"
 #include "asm.h"
-#include "../../Onegin/str.h"
-
-#define ISSPACE(text) while (isspace (*(text)))\
-                      {\
-                          (text)++;\
-                      }
-#define DEF_JMP(name, num, arg, arg_name,...) DEF_CMD(name, num, arg, arg_name, __VA_ARGS__)
-#define DEF_CMD(name, num, arg, arg_name,...) if (stricmp (cmd, #name) == 0)\
-                                          {\
-                                              op_code[ip++] = num;\
-                                              if (arg)\
-                                              {\
-                                                  assemble_##arg_name##_arg (&text, op_code, &ip, label, &label_index, &err);\
-                                              }\
-                                              if (err)\
-                                              {\
-                                                printf ("\nsyntax error\n");\
-                                                break;\
-                                              }\
-                                          }\
-                                          else
+#include "..\..\Onegin/str.h"
+#include "..\binary.h"
+#include "asm_strings.h"
+static const int ARR_SIZE = 100;
+static const int REG_LEN  = 5;
 
 int init_code (char *text, int *op_code, Label *label)
 {
@@ -35,19 +19,43 @@ int init_code (char *text, int *op_code, Label *label)
     while (*(text + 1) != '\0')
     {
         int temp = 0;
-        char cmd[100] = {};
+        char cmd[ARR_SIZE] = {};
 
         sscanf (text, "%s%n", cmd, &temp);
         text += temp + 1;
         temp = 0;
         fprintf (stderr, "command is {%s}\n", cmd);
 
-        if (isspace_s (cmd))
+        if (is_empty_str (cmd))
         {
             continue;
         }
 
-        #include "../cmd.h"
+#define DEF_JMP(name, num, arg, arg_name,...)               \
+        DEF_CMD(name, num, arg, arg_name, __VA_ARGS__)
+
+#define DEF_CMD(name, num, arg, arg_name,...)               \
+if (stricmp (cmd, #name) == 0)                              \
+{                                                           \
+    op_code[ip++] = num;                                    \
+    if (arg)                                                \
+    {                                                       \
+        assemble_##arg_name##_arg                           \
+        (&text, op_code, &ip, label, &label_index, &err);   \
+    }                                                       \
+    if (err)                                                \
+    {                                                       \
+        printf ("\nsyntax error\n");                        \
+        break;                                              \
+    }                                                       \
+}                                                           \
+else
+
+#include "../cmd.h"
+
+#undef DEF_CMD
+#undef DEF_JMP
+
         /*else*/ if (strchr (cmd, ':'))
         {
             assemble_label_arg (cmd, &ip, label, &label_index, &err);
@@ -63,25 +71,6 @@ int init_code (char *text, int *op_code, Label *label)
     return ip;
 }
 
-#undef DEF_CMD
-#undef DEF_JMP
-
-int isspace_s (char *arr)
-{
-    while (*arr != '\0')
-    {
-        if (!(isspace (*arr)))
-        {
-            return 0;
-        }
-        else
-        {
-            arr++;
-        }
-    }
-    return 1;
-}
-
 int is_label_name (Label *label, const char *jmp_name)
 {
     for (int i = 0; i < MAXSIZE && label->name[i] != nullptr; i++)
@@ -95,84 +84,83 @@ int is_label_name (Label *label, const char *jmp_name)
     return 0;
 }
 
-void print_op_code (FILE *out_file, int *op_code, int number, const int file_id, const int version)
+
+int assemble_push_arg (char **text, int *op_code, int *ip, Label *label, int *label_index, int *err)
 {
-    struct Head head = {};
-
-    head.file_id = file_id;
-    head.file_version = version;
-    head.number = number;
-
-    fwrite (&head, sizeof (struct Head), 1, out_file);
-    fwrite (op_code, sizeof (int), number, out_file);
-}
-
-void assemble_push_arg (char **text, int *op_code, int *ip, Label *label, int *label_index, int *err)
-{
-    int start_ip = --(*ip);
+    int start_ip = *ip - 1;
     int temp = 0;
-    char *pmem = strchr (*text, ']');
-    int sign = 1;
+    char arg[ARR_SIZE] = {0};
+    char reg[REG_LEN]  = {0};
+    char sign = 1;
+    int val = 0;
+    char *end_cmd = nullptr;
 
-    if (**text == '[' && pmem > *text)
+    if (!(sscanf (*text, "%s", arg)))
     {
-        op_code[start_ip] |= ARG_MEM;
+        fprintf (stderr, "ERROR: cannot read push argument");
 
+        return 0;
+    }
+
+    if (*arg == '[')
+    {
+        int last_smbl = strlen ((const char *)arg);
+
+        if (arg[last_smbl] == ']')
+        {
+            op_code[start_ip] |= ARG_MEM;
+            end_cmd = *text + last_smbl;
+            *(end_cmd) = '\0';
+
+            (*text)++;
+            skip_spaces (text);
+        }
+        else
+        {
+            printf ("ERROR: sintax error, '[' without ']'");
+            return 0;
+        }
+    }
+    if (**text == '-' || **text == '+')
+    {
+        if (**text == '-')
+        {
+            sign *= -1;
+        }
         (*text)++;
-
-        *pmem = '\0';
-        ISSPACE (*text);
+        skip_spaces (text);
     }
-    else
+    if (sscanf (*text, "%d%n", &val, &temp))
     {
-        pmem = nullptr;
-    }
-
-    if (**text == '-')
-    {
-        sign = -1;
-        (*text)++;
-        ISSPACE (*text);
-    }
-    else if (**text == '+')
-    {
-        (*text)++;
-        ISSPACE (*text);
-    }
-
-    if (isdigit (**text))
-    {
-        int val = 0;
-
-        sscanf (*text, "%d%n", &val, &temp);
-
         op_code[start_ip] |= ARG_IMMED;
-        (*ip)++;
-        op_code[*ip] = sign * val;
+
+        val *= sign;
+        op_code[(*ip)++] = val;
 
         *text += temp;
-
         temp = 0;
-        ISSPACE (*text);
-    }
-    if ((*text < pmem || pmem == nullptr) && (!(op_code[start_ip] & ARG_IMMED) && isalpha (**text)) || ((op_code[start_ip] & ARG_IMMED > 0) && **text == '+'))
-    {
-        char reg[5] = {};
+        skip_spaces (text);
 
-        if (**(text) == '+')
+        if (**text == '+')
         {
             (*text)++;
-            ISSPACE (*text);
-        }
+            skip_spaces (text);
 
-        sscanf (*text, "%s%n\n", reg, &temp);
-        if (reg > pmem && pmem != nullptr)
+            op_code[start_ip] |= ARG_REGISTR;
+        }
+    }
+    if ((sscanf (*text, "%s%n", reg, &temp))
+        && (((op_code[start_ip] & ARG_MEM > 0) && *text < end_cmd)
+        || (!(op_code[start_ip] & ARG_IMMED))))
+    {
+        if (!(isalpha (**text)))
         {
-            goto syntax_error;
-        }
-        op_code[start_ip] |= ARG_REGISTR;
+            fprintf (stderr, "ERROR: wrong register name");
 
-        (*ip)++;
+            return 0;
+        }
+
+        op_code[start_ip] |= ARG_REGISTR;
 
         if (stricmp (reg, "RAX") == 0)
         {
@@ -192,27 +180,38 @@ void assemble_push_arg (char **text, int *op_code, int *ip, Label *label, int *l
         }
         else
         {
-            syntax_error:
-            temp = 0;
-            *err = 1;               ////////
+            fprintf (stderr, "ERROR: sytax error, register with this name doesn't exist");
+            return 0;
         }
+
+        (*ip)++;
+
         *text += temp;
 
-        temp = 0;
+        if (op_code[start_ip] & ARG_MEM)
+        {
+            (*text)++;
+        }
     }
-
-    (*ip)++;
-
-    if (pmem)
+    else if (op_code[start_ip] & ARG_REGISTR)
     {
-        *pmem = ']';
-        *text = pmem + 1;
+        fprintf (stderr, "ERROR: syntax error, no register argument after '+'" );
+
+        return 0;
     }
+
+    if (op_code[start_ip] & ARG_MEM)
+    {
+        *end_cmd = ']';
+    }
+
+    return 0;
 }
+
 
 void assemble_jmp_arg (char **text, int *op_code, int *ip, Label *label, int *label_index, int *err)
 {
-    char jmp_name[100] = {};
+    char jmp_name[ARR_SIZE] = {};
     int temp_label_index = 0;
     int temp = 0;
 
@@ -233,10 +232,10 @@ void assemble_jmp_arg (char **text, int *op_code, int *ip, Label *label, int *la
 
         op_code[(*ip)++] = UNDEFINED;
     }
-    else
+    /*else
     {
         printf ("ERROR: label [%s] does not exist\n", jmp_name);
-    }
+    }*///  if label is called twice or more it is not an error, but prints error, printing must be only in second init
 }
 
 int assemble_label_arg (char *cmd, int *ip, Label *label, int *label_index, int *err)
@@ -262,10 +261,10 @@ int assemble_label_arg (char *cmd, int *ip, Label *label, int *label_index, int 
     {
         label->value[--temp_label_index] = *ip;
 
-        if (temp_label_index > *label_index)
+       /* if (temp_label_index > *label_index)
         {
             *label_index = temp_label_index + 1;
-        }
+        }*/
     }
 
     if (pname)
@@ -276,3 +275,14 @@ int assemble_label_arg (char *cmd, int *ip, Label *label, int *label_index, int 
     return temp_label_index;
 }
 
+void write_opcode (FILE *out_file, int *op_code, int n_instr, const int magic_number, const int version)
+{
+    struct Header header = {};
+
+    header.magic_number = magic_number;
+    header.file_version = version;
+    header.n_instr = n_instr;
+
+    fwrite (&header, sizeof (struct Header), 1, out_file);
+    fwrite (op_code, sizeof (int), n_instr, out_file);
+}
--- asm/asm.h
+++ asm/asm.h
@@ -7,6 +7,7 @@ static const int MAXLEN = 20;
 static const int UNDEFINED = -1;
 
 static const int LABEL_NAME = 0;
+static const int ERROR = 0;
 
 struct Label
 {
@@ -15,12 +16,12 @@ struct Label
     int status[MAXSIZE] = {};
 };
 
-int isspace_s (char *arr);
+
 int is_label_name (Label *label, const char *jmp_name);
 int init_code (char *text, int *op_code, Label *label);
-void print_op_code (FILE *out_file, int *op_code, int number, const int file_id, const int version);
+void write_opcode (FILE *out_file, int *op_code, int n_instr, const int magic_number, const int version);
 void assemble_pop_arg (char **text, int *op_code, int *ip, Label *label, int *label_index, int *err);
-void assemble_push_arg (char **text, int *op_code, int *ip, Label *label, int *label_index, int *err);
+int assemble_push_arg (char **text, int *op_code, int *ip, Label *label, int *label_index, int *err);
 void assemble_jmp_arg (char **text, int *op_code, int *ip, Label *label, int *label_index, int *err);
 int assemble_label_arg (char *cmd, int *ip, Label *label, int *label_index, int *err);
 
