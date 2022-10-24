6ad1741a10cb7a93a110c2515ee14eb797d26c8d
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-10-24 21:08:18 +0300

    first version of disassembler

--- disasm/disasm.h
+++ disasm/disasm.h
@@ -1,1 +1,10 @@
-mfsd
+#ifndef DISASM_H
+#define DISASM_H
+
+void diasm_code (const char *disasm, Label *lalbels, Header header, int *instrs);
+void process_push_arg (FILE *disasm_file, Label *labels, int cmd, int *instrs, int *index, int *label_index);
+void process_jmp_arg (FILE *disasm_file, Label *labels, int cmd, int *instrs, int *index, int *label_index);
+int find_label (Label *labels, int value);
+
+
+#endif /* DISASM_H */
--- disasm/main.cpp
+++ disasm/main.cpp
@@ -4,6 +4,12 @@
 
 #include "../../standart_functions/io/io.h"
 #include "../calc.h"
+#include "../binary.h"
+#include "../labels.h"
+#include "disasm.h"
+
+void process_push_arg (FILE *disasm_file, int cmd, int *instrs, int *index);
+void process_jmp_arg (FILE *disasm_file, int cmd, int *instrs, int *index);
 
 
 int main (int argc, const char **argv)
@@ -14,100 +20,31 @@ int main (int argc, const char **argv)
     }
 
     FILE *asm_file = fopen (argv[1], "rb");
-    FILE *disasm_file = fopen (argv[2], "wb");
 
-    assert (asm_file && disasm_file);
+    assert (asm_file);//error
 
+    struct Header header = {};
+    fread (&header, sizeof (header), 1, asm_file);
 
+    int *instrs = (int *)calloc (header.n_instr, sizeof (int));
 
+    assert (instrs);//error
 
+    fread (instrs, sizeof (int), header.n_instr, asm_file);
 
-    int asm_size = get_file_size (argv[1]);
+    Label labels = {};
 
-    char *text = (char *)calloc (asm_size + 2, sizeof (char));
+    diasm_code (argv[2], &labels, header, instrs);
+    diasm_code (argv[2], &labels, header, instrs);
 
-    int nlines = 0;
-    int verbose = 0;    //debug
+    FILE *disasm_file = fopen (argv[2], "a");
+    assert (disasm_file); // error
 
-    read_in_buf (asm_file, text, &nlines, asm_size, verbose);   //
+    fprintf (disasm_file, "magic_number is %o\nversion is %d\nnumber of instr is %d\n", header.magic_number, header.file_version, header.n_instr);
 
-    char *file_id = (char *)calloc (3, sizeof (char));
-    char *file_version = (char *)calloc (4, sizeof (char));
-    int number = 0;
+    fclose  (disasm_file);
 
-    int ip = 0;
-    sscanf (text, "%s%s%d%n", file_id, file_version, &number, &ip);
+    free (instrs);
 
-    fprintf (disasm_file, "%s\n%s\n%d\n", file_id, file_version, number);
-
-    int *asm_code = (int *)calloc (number, sizeof (int));
-
-    int index = 0;
-    for (index = 0; index < number; index++)
-    {
-        int temp = 0;
-        sscanf (text + ip, "%d%n", asm_code + index, &temp);
-        iv p += temp;
-    }
-
-    index = 0;
-    while (index < number)
-    {
-        switch (asm_code[index])
-        {
-            case CMD_PUSH:
-            {
-                fprintf (disasm_file, "push %d\n", asm_code[++index]);
-                index++;
-
-                break;
-            }
-            case CMD_SUB:
-            {
-                fprintf (disasm_file, "sub\n");
-                index++;
-
-                break;
-            }
-            case CMD_ADD:
-            {
-                fprintf (disasm_file, "add\n");
-                index++;
-
-                break;
-            }
-            case CMD_MULT:
-            {
-                fprintf (disasm_file, "mult\n");
-                index++;
-
-                break;
-            }
-            case CMD_DIV:
-            {
-                fprintf (disasm_file, "div\n");
-                index++;
-
-                break;
-            }
-            case CMD_OUT:
-            {
-                fprintf (disasm_file, "out\n");
-                index++;
-
-                break;
-            }
-            case CMD_HLT:
-            {
-                fprintf (disasm_file, "hlt");
-                index = number;
-
-                break;
-            }
-        }
-    }
-
-    free (file_id);
-    free (file_version);
-    free (asm_code);
+    return 0;
 }
